f327791ad63fe1b6c87077a1b5454a9f2376e082
austintedwards <austintedwards@gmail.com>
2017-11-07 09:26:59 -0700

    adding google geolocator

--- app.js
+++ app.js
@@ -8,15 +8,19 @@ let location_array = []
     $(".location-list").append(`<div class = '${new_local}'><li class ='col s10'>${new_local}</li><button class ='col s2 btn-flat remove-location'>X</button></div>`)
     location_array.push(new_local)
     $(".location").val('')
-    console.log(location_array)
   })
 
   $(".location-list").click((event)=>{
     let text = $(event)["0"].target.parentNode.className
     var index = location_array.indexOf(text);
     if (index > -1) location_array.splice(index, 1)
-    console.log(location_array)
   })
+
+const ACCESS_KEY = 'AIzaSyAj8bYpM2M0T8I4xRKqvToSn9_fsRYC6dc'
+
+$.get(`https://maps.googleapis.com/maps/api/geocode/json?address=1600+Amphitheatre+Parkway,+Mountain+View,+CA&key=${ACCESS_KEY}`, data =>{
+  console.log(data)
+})
 
 });
 
