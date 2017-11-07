98cda3bc850cbc75fcfe212d5fec8ac6cf25593d
austintedwards <austintedwards@gmail.com>
2017-11-07 09:35:57 -0700

    add sort button and fix git call error

--- app.js
+++ app.js
@@ -1,5 +1,6 @@
 $(()=> {
 let location_array = []
+let geocode_array=[]
   // Initialize collapse button
   $(".button-collapse").sideNav({});
 
@@ -7,6 +8,7 @@ let location_array = []
     let new_local = $(".location").val()
     $(".location-list").append(`<div class = '${new_local}'><li class ='col s10'>${new_local}</li><button class ='col s2 btn-flat remove-location'>X</button></div>`)
     location_array.push(new_local)
+    google_locations(new_local)
     $(".location").val('')
   })
 
@@ -16,11 +18,26 @@ let location_array = []
     if (index > -1) location_array.splice(index, 1)
   })
 
+  $(".sort-elevations").click(() => {
+    if (location_array.length>1){
+      locations
+    }
+})
+
+
 const ACCESS_KEY = 'AIzaSyAj8bYpM2M0T8I4xRKqvToSn9_fsRYC6dc'
 
-$.get(`https://maps.googleapis.com/maps/api/geocode/json?address=1600+Amphitheatre+Parkway,+Mountain+View,+CA&key=${ACCESS_KEY}`, data =>{
-  console.log(data)
-})
+let google_locations = (local)=>{
+  $.get(`https://maps.googleapis.com/maps/api/geocode/json?address=${local}&key=${ACCESS_KEY}`, data =>{
+  })
+  .done((data)=>{
+    console.log(data)
+  })
+  .fail(()=> {
+    console.log( "error" );
+  })
+}
+
 
 });
 
